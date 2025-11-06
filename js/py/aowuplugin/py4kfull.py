2a9c479a6f51f9b6f0b61b4a962ee6df2118c750
zzzzqs222-png <zzzzqs222@gmail.com>
2025-11-06 15:21:16 +0800

    Update py4kfull.py

--- js/py/aowuplugin/py4kfull.py
+++ js/py/aowuplugin/py4kfull.py
@@ -116,29 +116,7 @@ class Spider(Spider):
         return {'list': self.getlist(data(".margin-fix .item")), 'page': pg}
 
     def playerContent(self, flag, id, vipFlags):
-        try:
-            data=self.getpq(id)
-            p,url=0,data('video id > source:first-of-type').attr('src')
-            if not url:raise Exception("未找到播放地址")
-        except Exception as e:
-            p,url=1,f"{self.host}{id}"
-        headers = {
-            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.5410.0 Safari/537.36',
-            'pragma': 'no-cache',
-            'cache-control': 'no-cache',
-            'sec-ch-ua-platform': '"Windows"',
-            'sec-ch-ua': '"Not(A:Brand";v="99", "Google Chrome";v="133", "Chromium";v="133"',
-            'dnt': '1',
-            'sec-ch-ua-mobile': '?0',
-            'origin': self.host,
-            'sec-fetch-site': 'cross-site',
-            'sec-fetch-mode': 'cors',
-            'sec-fetch-dest': 'empty',
-            'referer': f'{self.host}/',
-            'accept-language': 'zh-CN,zh;q=0.9,en;q=0.8',
-            'priority': 'u=1, i',
-        }
-        return  {'parse': p, 'url': url, 'header': headers}
+        return {"parse": 0, "url": id, "header": {'User-Agent': 'User-Agent: Lavf/58.12.100'}}
 
     def localProxy(self, param):
         pass
@@ -218,6 +196,7 @@ class Spider(Spider):
         vhtml = data("script[type='application/ld+json']").text()
         jst = json.loads(vhtml.split('initials=')[-1][:-1])
         return jst
+
 
 
 
