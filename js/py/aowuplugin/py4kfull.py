cea49e97700e4e2c0b1222ebef84c53f0b668cb9
zzzzqs222-png <zzzzqs222@gmail.com>
2025-11-07 14:44:55 +0800

    Update py4kfull.py

--- js/py/aowuplugin/py4kfull.py
+++ js/py/aowuplugin/py4kfull.py
@@ -1,3 +1,9 @@
+"""
+
+作者 乐哥 🚓 内容均从互联网收集而来 仅供交流学习使用 版权归原创者所有 如侵犯了您的权益 请通知作者 将及时删除侵权内容
+                    ====================lege====================
+
+"""
 
 import requests
 from bs4 import BeautifulSoup
@@ -37,10 +43,71 @@ class Spider(Spider):
     def manualVideoCheck(self):
         pass
 
+    def extract_middle_text(self, text, start_str, end_str, pl, start_index1: str = '', end_index2: str = ''):
+        if pl == 3:
+            plx = []
+            while True:
+                start_index = text.find(start_str)
+                if start_index == -1:
+                    break
+                end_index = text.find(end_str, start_index + len(start_str))
+                if end_index == -1:
+                    break
+                middle_text = text[start_index + len(start_str):end_index]
+                plx.append(middle_text)
+                text = text.replace(start_str + middle_text + end_str, '')
+            if len(plx) > 0:
+                purl = ''
+                for i in range(len(plx)):
+                    matches = re.findall(start_index1, plx[i])
+                    output = ""
+                    for match in matches:
+                        match3 = re.search(r'(?:^|[^0-9])(\d+)(?:[^0-9]|$)', match[1])
+                        if match3:
+                            number = match3.group(1)
+                        else:
+                            number = 0
+                        if 'http' not in match[0]:
+                            output += f"#{'📽️' + match[1]}${number}{xurl}{match[0]}"
+                        else:
+                            output += f"#{'📽️' + match[1]}${number}{match[0]}"
+                    output = output[1:]
+                    purl = purl + output + "$$$"
+                purl = purl[:-3]
+                return purl
+            else:
+                return ""
+        else:
+            start_index = text.find(start_str)
+            if start_index == -1:
+                return ""
+            end_index = text.find(end_str, start_index + len(start_str))
+            if end_index == -1:
+                return ""
+
+        if pl == 0:
+            middle_text = text[start_index + len(start_str):end_index]
+            return middle_text.replace("\\", "")
+
+        if pl == 1:
+            middle_text = text[start_index + len(start_str):end_index]
+            matches = re.findall(start_index1, middle_text)
+            if matches:
+                jg = ' '.join(matches)
+                return jg
+
+        if pl == 2:
+            middle_text = text[start_index + len(start_str):end_index]
+            matches = re.findall(start_index1, middle_text)
+            if matches:
+                new_list = [f'✨{item}' for item in matches]
+                jg = '$$$'.join(new_list)
+                return jg
+
     def homeContent(self, filter):
         result = {}
-        result = {"class": [{"type_id": "latest-updates", "type_name": "最新"},
-                            {"type_id": "top-rated", "type_name": "最佳视频🌠"},
+        result = {"class": [{"type_id": "latest-updates", "type_name": "最新视频🌠"},
+                            {"type_id": "top-rated", "type_name": "最佳视频"},
                             {"type_id": "most-popular", "type_name": "热门影片🌠"}],
                  }
 
